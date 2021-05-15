02038a1c5d42d43bb7d91a03d2049e3af0e17615
gubriansky@sernato.sk <gubriansky@sernato.sk>
2021-05-15 15:53:13 +0200

    update to persist any changes to character not just health

--- client/system/persistentCharacter.js
+++ client/system/persistentCharacter.js
@@ -2,6 +2,7 @@ if(config.world.persistCharacters) {
     MRP_CLIENT.CreateThread(()=>{
         let cycle = async function(){
             let lastEntityHealth = 0;
+            let lastCharacter = null;
 
             while(true){
                 await MRP_CLIENT.sleep(0);
@@ -9,13 +10,13 @@ if(config.world.persistCharacters) {
                 let playerPed = PlayerPedId();
 
                 let currentHealth = GetEntityHealth(playerPed);
+                let character = MRP_CLIENT.GetPlayerData();
+                if(!character) {
+                    await MRP_CLIENT.sleep(500);
+                    continue;
+                }
+                
                 if(lastEntityHealth != currentHealth) {
-                    let character = MRP_CLIENT.GetPlayerData();
-                    if(!character) {
-                        await MRP_CLIENT.sleep(500);
-                        continue;
-                    }
-
                     if(character.sex == "MALE") {
                         //because reasons :D
                         character.stats.health = currentHealth - 100;
@@ -24,6 +25,20 @@ if(config.world.persistCharacters) {
                     }
                     lastEntityHealth = currentHealth;
                     emitNet('mrp:updateCharacter', character);
+                    emit('mrp:updateCharacter', character);
+                } else {
+                    if(lastCharacter == null)
+                        lastCharacter = character;
+                    
+                    let lastCharString = JSON.stringify(lastCharacter);
+                    let curCharString = JSON.stringify(character);
+                    if(lastCharString != curCharString) {
+                        //something changed send update
+                        emitNet('mrp:updateCharacter', character);
+                        emit('mrp:updateCharacter', character);
+                    }
+                    
+                    lastCharacter = character;
                 }
 
                 await MRP_CLIENT.sleep(500);
