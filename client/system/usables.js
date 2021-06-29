37f3d11176b245b85d1ecc34b68cf00674b9e636
mufty <gubriansky@sernato.sk>
2021-06-29 16:56:04 +0200

    firstaid and bandages

--- client/system/usables.js
+++ client/system/usables.js
@@ -105,12 +105,23 @@ onNet('mrp:client:useables:use', (usable) => {
                             moveRate = DEFAULT_MOVE_RATE;
                         }, use.duration);
                     }
-                } else if (use.name == 'hunger' || use.name == 'thirst' || use.name == 'stress') {
+                } else if (use.name == 'hunger' || use.name == 'thirst' || use.name == 'stress' || use.name == 'armor') {
                     let tick = use.tick;
                     if (tick) {
                         let count = 0;
                         let interval = setInterval(() => {
                             addStat(use.name, tick.factor);
+                            count++;
+                            if (count >= tick.count)
+                                clearInterval(interval);
+                        }, tick.tickCadence);
+                    }
+                } else if (use.name == 'health') {
+                    let tick = use.tick;
+                    if (tick) {
+                        let count = 0;
+                        let interval = setInterval(() => {
+                            addHealth(tick.factor);
                             count++;
                             if (count >= tick.count)
                                 clearInterval(interval);
