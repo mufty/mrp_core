8c878786f340dd74c3c952e064c09f2b702dbf52
mufty <gubriansky@sernato.sk>
2021-08-25 18:22:39 +0200

    added debug using convar

--- shared/debug.js
+++ shared/debug.js
@@ -8,7 +8,19 @@ mrp_logger = {
             nativeLog('^2' + RESOURCE_NAME + '::^5' + date.toUTCString() + ':: ^1' + err + '^7');
         else
             nativeLog('^2' + RESOURCE_NAME + '::^5' + date.toUTCString() + ':: ^1' + err + '^7', args);
+    },
+    debug: function(err, ...args) {
+        let debugOn = GetConvar('mrp_debug', 'false');
+        if (debugOn == 'false')
+            return;
+
+        let date = new Date();
+        if (!args || args.length < 1)
+            nativeLog('^2' + RESOURCE_NAME + '::^5' + date.toUTCString() + ':: ^1' + err + '^7');
+        else
+            nativeLog('^2' + RESOURCE_NAME + '::^5' + date.toUTCString() + ':: ^1' + err + '^7', args);
     }
 };
 
 console.log = mrp_logger.log;
+console.debug = mrp_logger.debug;
